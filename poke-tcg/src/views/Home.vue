80a678c36b339b0bbf0bda2cc95bd82a202f7517
iuritb <iuri.t.barros@gmail.com>
2022-09-27 18:31:39 -0300

    feat: add pokeapi and configure it

--- poke-tcg/src/views/Home.vue
+++ poke-tcg/src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
     <h1>Bem vindo ao mundo do PKM TCG</h1>
-    <pre>
-      {{ cards }}
-    </pre>
   </div>
 </template>
 
@@ -12,6 +9,7 @@ import Vue from "vue";
 
 //API
 import apiTcg from "@/services/cards";
+import pokeApi from "@/services/pokemon";
 
 export default Vue.extend({
   name: "View-Home",
@@ -19,20 +17,38 @@ export default Vue.extend({
   components: {},
   data() {
     return {
-      cards: {} as object,
+      allCards: {} as object,
+      cardsByName: {} as object,
+      pokemon: {} as object,
     };
   },
 
   mounted() {
     this.loadCards();
+    this.loadCardsByName();
+    this.loadPokemon();
   },
 
   methods: {
     async loadCards() {
       console.log("loadCards");
-      await apiTcg.getAllCards("name:charizard").then(({ data }) => {
-        console.log("cards =>", data);
-        this.cards = data;
+      await apiTcg.getAllCards().then(({ data }) => {
+        console.log("all cards =>", data);
+        this.allCards = data;
+      });
+    },
+    async loadCardsByName() {
+      console.log("loadCardByName");
+      await apiTcg.getCardsByName("charizard").then(({ data }) => {
+        console.log("cards by name =>", data);
+        this.cardsByName = data;
+      });
+    },
+    async loadPokemon() {
+      console.log("loadPokemon");
+      await pokeApi.getAllPokemon().then(({ data }) => {
+        console.log("pokemon list =>", data);
+        this.pokemon = data;
       });
     },
   },
